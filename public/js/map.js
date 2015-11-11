4119c399a974b37ea8fc884358cbc5551f520831
nadapapa <nadapapa@gmail.com>
2015-11-11 10:57:33 +0100

    map displays the world and hex can be highlighted

--- public/js/map.js
+++ public/js/map.js
@@ -3,7 +3,7 @@ var highlight1 = L.icon({
 
     iconSize: [9, 9], // size of the icon
 
-    iconAnchor: [4.5, 4.5] // point of the icon which will correspond to marker's location
+    iconAnchor: [0, 0] // point of the icon which will correspond to marker's location
     //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
@@ -12,7 +12,7 @@ var highlight2 = L.icon({
 
     iconSize: [18, 18], // size of the icon
 
-    iconAnchor: [9, 9] // point of the icon which will correspond to marker's location
+    iconAnchor: [0, 0] // point of the icon which will correspond to marker's location
     //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
@@ -21,7 +21,7 @@ var highlight3 = L.icon({
 
     iconSize: [36, 36], // size of the icon
 
-    iconAnchor: [18, 18] // point of the icon which will correspond to marker's location
+    iconAnchor: [0, 0] // point of the icon which will correspond to marker's location
     //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
@@ -30,9 +30,16 @@ var highlight4 = L.icon({
 
     iconSize: [72, 72], // size of the icon
 
-    iconAnchor: [36, 36] // point of the icon which will correspond to marker's location
+    iconAnchor: [0, 0] // point of the icon which will correspond to marker's location
     //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
+
+// some constants
+var margin_x = 27;
+var margin_y = 36;
+var HEX_HEIGHT = 72;
+var HEX_SCALED_HEIGHT = HEX_HEIGHT * 1.0;
+var HEX_SIDE = HEX_SCALED_HEIGHT / 2;
 
 function init() {
     var mapMinZoom = 1;
@@ -52,8 +59,8 @@ function init() {
     rc = new L.RasterCoords(map, img);
 
     var mapBounds = new L.LatLngBounds(
-        map.unproject([0, 2231], mapMaxZoom),
-        map.unproject([2988, 0], mapMaxZoom));
+        map.unproject([0, 3000], mapMaxZoom),
+        map.unproject([3000, 0], mapMaxZoom));
 
     map.fitBounds(mapBounds);
 
@@ -77,7 +84,8 @@ function init() {
         position: 'topright',
         height: 200
     }).addTo(map);
-
+    map.panBy([1, 0]);
+    //L.Util.requestAnimFrame(map.invalidateSize, map, !1, map._container);
     map.on('click', onMapClick);
 
     map.on('zoomend', function () {
@@ -102,11 +110,53 @@ function init() {
 }
 
 function onMapClick(e) {
+    var x = (rc.project(e.latlng).x - margin_x - (HEX_HEIGHT / 2)) / (HEX_HEIGHT * 0.75);
+    var y = (rc.project(e.latlng).y - margin_y - (HEX_HEIGHT / 2)) / HEX_HEIGHT;
+    var z = -0.5 * x - y;
+    var y = -0.5 * x + y;
+
+    ix = Math.floor(x + 0.5);
+    iy = Math.floor(y + 0.5);
+    iz = Math.floor(z + 0.5);
+    s = ix + iy + iz;
+    if (s) {
+        abs_dx = Math.abs(ix - x);
+        abs_dy = Math.abs(iy - y);
+        abs_dz = Math.abs(iz - z);
+        if (abs_dx >= abs_dy && abs_dx >= abs_dz) {
+            ix -= s;
+        } else if (abs_dy >= abs_dx && abs_dy >= abs_dz) {
+            iy -= s;
+        } else {
+            iz -= s;
+        }
+    }
+
+    // map_x and map_y are the map coordinates of the click
+    map_x = ix;
+    map_y = (iy - iz + (1 - ix % 2 ) ) / 2 - 0.5;
+
+    if (map_x < 0 || map_y < 0 || map_x > 39 || map_y > 39) {
+        return false;
+    }
+
     console.log(rc.project(e.latlng));
+
+    console.log(map_x + ", " + map_y);
+
+    // --- Calculate coordinates of this hex.  We will use this
+    // --- to place the highlight image.
+
+    tx = map_x * HEX_SIDE * 1.5;
+    ty = map_y * HEX_SCALED_HEIGHT + (map_x % 2) * HEX_SCALED_HEIGHT / 2;
+
+
+
 
     if (typeof highlightMarker != 'undefined') {
         map.removeLayer(highlightMarker);
     }
+
     switch (map.getZoom()) {
         case 1:
             var highlight = highlight1;
@@ -121,5 +171,6 @@ function onMapClick(e) {
             var highlight = highlight4;
             break;
     }
-    highlightMarker = L.marker(e.latlng, {icon: highlight}).addTo(map);
+
+    highlightMarker = L.marker(rc.unproject([tx + margin_x, ty + margin_y]), {icon: highlight}).addTo(map);
 }
